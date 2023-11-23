e55413765c8ce70e94edce3b50052501f2176d8c
Kyron Paraense <144981092+zaratt0@users.noreply.github.com>
2023-11-22 22:18:43 -0300

    Update ContaTerminal.java

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+         System.out.println("Digite o número da conta?");
+        int numeroConta = scanner.nextInt();
+
+        String agencia = "067-8";
+        String nomeCliente = "Kyron Paraense";
+        double saldo = 237.48;
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é: " + agencia);
+        System.out.println("Número da conta: " + numeroConta);
+        System.out.println("Seu saldo disponível para saque é: " + saldo);
     }
 }
